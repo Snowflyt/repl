32797baae95df77c4eaaff59d9a69af3a356f55d
Snowflyt <gaoge011022@163.com>
2025-10-25 16:54:04 +0800

    ✨ feat(CDN): Switch to esm.sh for better bundling control
    jsdelivr emits corrupted bundles for ECharts. Truly mysterious.

--- src/stores/sandbox.ts
+++ src/stores/sandbox.ts
@@ -177,7 +177,6 @@ const sandboxStore = create({
           import("../utils/sandbox")
             .then(({ Sandbox }) => {
               sandbox = new Sandbox();
-              void sandbox.checkCdnAccessibility();
               mockConsole();
               this.isLoading = false;
             })
@@ -187,7 +186,6 @@ const sandboxStore = create({
 
     const { Sandbox } = await import("../utils/sandbox");
     sandbox = new Sandbox();
-    void sandbox.checkCdnAccessibility();
     mockConsole();
     this.isLoading = false;
   },
